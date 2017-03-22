b7d2fde527267a99699b1c4dc8a810de6c792e6f
Daniel Mermelstein <danmermel@gmail.com>
2017-03-22 13:55:12 +0000

    it works

--- boxoffice.js
+++ boxoffice.js
@@ -25,13 +25,9 @@ var Web3 = require('web3');
 var web3 = new Web3();
 var async = require('async');
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8000'));
-var gig_abi = [{"constant":true,"inputs":[],"name":"seat_count","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_seat_address1","type":"address"},{"name":"_seat_address2","type":"address"},{"name":"_seat_address3","type":"address"},{"name":"_seat_address4","type":"address"}],"name":"buy_seats","outputs":[],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"artist","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"event_owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"seating_plan","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"venue","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_seat_to_redeem","type":"address"}],"name":"redemption_challenge","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_seat_to_redeem","type":"address"}],"name":"redeem_ticket","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"seating_list","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_seat_address","type":"address"}],"name":"buy_seat","outputs":[],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"event_time","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"event_name","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"seats_sold","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_seat_name","type":"bytes32"},{"name":"_price","type":"uint256"},{"name":"_sellable_from","type":"uint256"},{"name":"_sellable_until","type":"uint256"}],"name":"create_seat","outputs":[],"payable":false,"type":"function"},{"inputs":[{"name":"_venue","type":"bytes32"},{"name":"_event_name","type":"bytes32"},{"name":"_event_time","type":"uint256"},{"name":"_artist","type":"address"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"seat_address","type":"address"}],"name":"Log_seat_created","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"seat_bought","type":"address"}],"name":"Log_seat_bought","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"ticket_redeemed","type":"address"},{"indexed":false,"name":"redeemer","type":"address"}],"name":"Log_ticket_redeemed","type":"event"}]; 
+var gig_abi = [{"constant":true,"inputs":[],"name":"seat_count","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_seat_address1","type":"address"},{"name":"_seat_address2","type":"address"},{"name":"_seat_address3","type":"address"},{"name":"_seat_address4","type":"address"}],"name":"buy_seats","outputs":[],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"artist","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"event_owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"seating_plan","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"venue","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_seat_to_redeem","type":"address"}],"name":"redemption_challenge","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_seat_to_redeem","type":"address"}],"name":"redeem_ticket","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"seating_list","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_seat_address","type":"address"}],"name":"buy_seat","outputs":[],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"event_time","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"event_name","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"seats_sold","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_seat_name","type":"bytes32"},{"name":"_price","type":"uint256"},{"name":"_sellable_from","type":"uint256"},{"name":"_sellable_until","type":"uint256"}],"name":"create_seat","outputs":[],"payable":false,"type":"function"},{"inputs":[{"name":"_venue","type":"bytes32"},{"name":"_event_name","type":"bytes32"},{"name":"_event_time","type":"uint256"},{"name":"_artist","type":"address"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"seat_address","type":"address"}],"name":"Log_seat_created","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"seat_address","type":"address"}],"name":"Log_seat_bought","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"seat_address","type":"address"},{"indexed":false,"name":"redeemer","type":"address"}],"name":"Log_ticket_redeemed","type":"event"}];
 var seat_abi = [{"constant":true,"inputs":[],"name":"redeemed_by_seat_owner","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"status","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"seat_name","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"artist","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"event_owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"sellable_from","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"mark_cancelled","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"price","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"venue","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"mark_unlocked","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_redeemer","type":"address"}],"name":"redeem_ticket","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"redeemed_by_event_owner","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_seat_owner","type":"address"}],"name":"buy_seat","outputs":[],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"event_time","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"sellable_until","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"seat_owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"event_name","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"inputs":[{"name":"_venue","type":"bytes32"},{"name":"_event_name","type":"bytes32"},{"name":"_seat_name","type":"bytes32"},{"name":"_price","type":"uint256"},{"name":"_event_time","type":"uint256"},{"name":"_artist","type":"address"},{"name":"_sellable_from","type":"uint256"},{"name":"_sellable_until","type":"uint256"}],"payable":false,"type":"constructor"}];
 var gig = web3.eth.contract(gig_abi).at(gig_address);
-console.log(gig);
-/*gig.event_name(function(err, data) {
-  console.log(err, data);
-});*/
 var events = gig.allEvents();
 
 var hex_to_ascii = function (str1) {  
@@ -54,12 +50,55 @@ var extractSeat = function(seat_id, callback) {
   async.parallel([
     function(done) {
       seat.venue(done);
+    },
+    function(done) {
+      seat.event_name(done);
+    },
+    function(done) {
+      seat.seat_name(done);
+    },
+    function(done) {
+      seat.price(done);
+    },
+    function(done) {
+      seat.event_time(done);
+    },
+    function(done) {
+      seat.event_owner(done);
+    },
+    function(done) {
+      seat.artist(done);
+    },
+    function(done) {
+      seat.seat_owner(done);
+    },
+    function(done) {
+      seat.sellable_from(done);
+    },
+    function(done) {
+      seat.sellable_until(done);
+    },
+    function(done) {
+      seat.redeemed_by_seat_owner(done);
+    },
+    function(done) {
+      seat.redeemed_by_event_owner(done);
     }
   ], function(err, data) {
     var obj = {  
-      venue: hex_to_ascii(data[0]) 
+      venue: hex_to_ascii(data[0]),
+      event_name: hex_to_ascii(data[1]),
+      seat_name: hex_to_ascii(data[2]),
+      price: parseInt(data[3].toString()),
+      event_time: parseInt(data[4].toString()),
+      event_owner: data[5].toString(),
+      artist: data[6].toString(),
+      seat_owner: data[7].toString(),
+      sellable_from: parseInt(data[8].toString()),
+      sellable_until: parseInt(data[9].toString()),
+      redeemed_by_seat_owner: data[10].toString() == 'true' ? true : false,
+      redeemed_by_event_owner: data[11].toString() == 'true' ? true : false
     };
-    console.log('obj', obj);
     callback(err, obj);
   });
  
@@ -73,7 +112,10 @@ events.watch(function(error, event) {
 //"transactionHash":"0x0000000000000000000000000000000000000000000000000000000000000000",
 //"blockHash":null,"logIndex":0,"removed":false,"event":"Log_seat_created","args":{"seat_address":"0x6593923764ff52643f3d9ec194749564c90dc8a7"}}
   extractSeat(event.args.seat_address, function(err, data) {
-    console.log('done');
+    console.log('seat', event.args.seat_address, data);
+    data._id = event.args.seat_address;
+    data.last_event = event.event;
+    db.update(data);
   });
 });
 
